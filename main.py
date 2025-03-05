341488610989f49dc5243eeade7f84caf7af41b6
Gaving328 <bob425645@gmail.com>
2025-03-05 23:29:36 +0000

    complete

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 print("Hello user!")
 name = input("What is your name? ")
 age = input("What is your age? ")
-print("How may I help you, " + name)
+print("How may I help you, " + name + "?")
 
-option = input("Would you like to see our settings, menu, exit, and has your food arived if not select 1 ").lower()
+option = input("Would you like to see our settings, menu, exit, or if your food hasn't arrived, select 1: ").lower()
+
 if option == "exit":
     print("Goodbye!")
     exit() 
 elif option == "1":
-    no_food = input("Would you want a refund or a re-delivery? ")
-    no_food == "refund":
-        print("You're order will be refunded thank you for your patience!")
-    no_food == "re-delivery"
-        print("Thank you for you patience you're food will be delivered soon!"
+    no_food = input("Would you want a refund or a re-delivery? ").lower()
+    
+    if no_food == "refund":
+        print("Your order will be refunded. Thank you for your patience!")
+    elif no_food == "re-delivery":
+        print("Thank you for your patience! Your food will be delivered soon!")
+    else:
+        print("Invalid option. Please select either 'refund' or 're-delivery'.")
+
